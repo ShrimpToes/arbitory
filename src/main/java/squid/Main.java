ddcd06c2ca6fcc15ba61ea19c91611a72ac69869
Ike <ikeschwetman@icloud.com>
2022-01-12 12:34:36 -0800

    changes to intellij project

--- src/main/java/squid/Main.java
+++ src/main/java/squid/Main.java
@@ -4,8 +4,6 @@ import squid.engine.Game;
 import squid.game.DummyGame;
 import squid.game.SpaceshipGame;
 
-import static java.lang.Thread.sleep;
-
 public class Main {
 
     public static void main(String[] args) {
